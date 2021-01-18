d77d642cea03659f04912bbebbc7d0b99a7f76b1
harrisgomez <node.harris@gmail.com>
2021-01-17 20:09:36 -0500

    Created retrieve fn. Built URI. Created fetch call to URI.

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -6,4 +6,32 @@ window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 
+const retrieve = options => {
+    // Options for accepted by the /records endpoint, accepted as query string params
+    const options = options || {};
+    const limit = 10;
+    const page = options.page || 1;
+
+    // Build the URI for the records API
+    const uri = URI(window.path)
+        .addSearch('limit', limit)
+        .addSearch('offset', limit * (page - 1));
+
+    // Add 'color[]' param if options contains any colors
+    if (options.colors && options.colors.length > 0) uri.addSearch('color[]', options.colors);
+
+    // Fetch call to the records API
+    return fetch(window.path)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Failed to fetch from Records API. Error code ' + res.status);
+            }
+        })
+        .then(data => {
+            // Transform the payload upon successful promise response
+        })
+}
+
 export default retrieve;
