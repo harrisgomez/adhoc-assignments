afcf1e22058d3a106082191494db7b007ef8e63c
harrisgomez <node.harris@gmail.com>
2021-01-17 23:04:22 -0500

    Transformed records payload to include: ids, open, closedPrimaryCount, previousPage, nextPage. Ran successful test.

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -6,32 +6,58 @@ window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 
-const retrieve = options => {
-    // Options for accepted by the /records endpoint, accepted as query string params
-    const options = options || {};
-    const limit = 10;
-    const page = options.page || 1;
+// Helper fn. Adds 'isPrimary' key to each item in records, then
+// sets the value to true/false depending on whether or not item contains a primary color.
+const setPrimaryColors = records => {
+    const primaryColors = ['red', 'blue', 'yellow'];
+
+    records.forEach(item => {
+        item.isPrimary = primaryColors.some(pColor => item.color.includes(pColor))
+    });
+
+    return records;
+};
+
+const retrieve = (options = {}) => {
+    // Options for the /records endpoint, accepted as query string params
+    let limit = 10;
+    let page = options.page || 1;
 
     // Build the URI for the records API
     const uri = URI(window.path)
-        .addSearch('limit', limit)
+        .addSearch('limit', limit + 1)
         .addSearch('offset', limit * (page - 1));
 
     // Add 'color[]' param if options contains any colors
     if (options.colors && options.colors.length > 0) uri.addSearch('color[]', options.colors);
 
     // Fetch call to the records API
-    return fetch(window.path)
+    return fetch(uri)
         .then(res => {
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw new Error('Failed to fetch from Records API. Error code ' + res.status);
-            }
+            if (!res.ok) throw new Error(res.status + '. Records API failed.');
+            return res.json();
         })
-        .then(data => {
+        .then(records => {
+            // Check items for primary colors
+            setPrimaryColors(records);
+
+            // Check if on the last page
+            const isLastPage = records.length <= limit;
+
+            if (!isLastPage) records.splice(limit, 1);
+
             // Transform the payload upon successful promise response
+            const result = {
+                ids: records.map(item => item.id),
+                open: records.filter(item => item.disposition === 'open'),
+                closedPrimaryCount: records.filter(item => item.disposition === 'closed' && item.isPrimary).length,
+                previousPage: page === 1 ? null : page - 1,
+                nextPage: isLastPage ? null : page + 1
+            };
+
+            return result;
         })
-}
+        .catch(console.log);
+};
 
 export default retrieve;
